6259c42a06b33d84dbcd37f0b0d9b689c639524b
mesteralfreddohnani <alfred.doh-nani@meltwater.org>
2019-12-31 13:32:10 +0000

    Fix EXCEPTION CAUGHT BY FOUNDATION error

--- lib/screens/onboarding/components/OnboardPage.dart
+++ lib/screens/onboarding/components/OnboardPage.dart
@@ -3,7 +3,7 @@ import 'package:kitchin/providers/ColorProvider.dart';
 import 'package:kitchin/screens/onboarding/components/DrawerPaint.dart';
 import 'package:kitchin/screens/onboarding/models/OnboardPageModel.dart';
 import 'package:provider/provider.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OnboardPage extends StatefulWidget{
   final PageController pageController;
@@ -24,6 +24,7 @@ class _OnboardPageState extends State<OnboardPage>
 
     @override
     void initState() {
+       super.initState();
       animationController = AnimationController(vsync: this, duration: new Duration(milliseconds: 750));
 
       heroAnimation = new Tween<double>(begin: -40, end: 0).animate(
@@ -34,7 +35,6 @@ class _OnboardPageState extends State<OnboardPage>
       );
 
       animationController.forward(from: 0);
-      super.initState();
     }
 
     @override
@@ -53,101 +53,104 @@ class _OnboardPageState extends State<OnboardPage>
       );
     }
 
+    @override
     Widget build(BuildContext context) {
-      return new Stack(
-        children: <Widget>[
-          new Container(
-            color: widget.pageModel.primeColor,
+    return Stack(
+      children: <Widget>[
+        Container(
+          color: widget.pageModel.primeColor,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-                AnimatedBuilder(
-            animation: heroAnimation,
-            builder: (BuildContext context, child) {
-              return new Transform.translate(
-                offset: new Offset(heroAnimation.value, 0),
-                child: new Padding(
-                  padding: const EdgeInsets.only(top: 32.0),
-                  child: Image.asset(widget.pageModel.imagePath),
-                ),
-              );
-            },
-          ),
-          new Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32.0),
-            child: new Container(
-              height: 250,
-              width: double.infinity,
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: new Text(
-                      widget.pageModel.caption,
-                      style: new TextStyle(
-                        fontSize: 24,
-                        color: widget.pageModel.accentColor.withOpacity(0.8),
-                        letterSpacing: 1,
-                        fontStyle: FontStyle.normal,
-                      ),
+              AnimatedBuilder(
+                animation: heroAnimation,
+                builder: (context, child) {
+                  return Transform.translate(
+                    offset: Offset(heroAnimation.value, 0),
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 32.0),
+                      child: SvgPicture.asset(widget.pageModel.imagePath),
                     ),
-                  ),
-                  new Padding(
-                    padding: const EdgeInsets.only(bottom: 8.0),
-                    child: new Text(
-                      widget.pageModel.subhead,
-                      style: new TextStyle(
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                        color: widget.pageModel.accentColor,
-                        letterSpacing: 1,
-                        fontStyle: FontStyle.normal,
-                      )
-                    )
-                  ),
-                  new Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: new Text(
-                      widget.pageModel.description,
-                      style: new TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: widget.pageModel.accentColor,
-                          )
+                  );
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Container(
+                  height: 250,
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          widget.pageModel.caption,
+                          style: TextStyle(
+                              fontSize: 24,
+                              color:
+                                  widget.pageModel.accentColor.withOpacity(0.8),
+                              letterSpacing: 1,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: Text(
+                          widget.pageModel.subhead,
+                          style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                              color: widget.pageModel.accentColor,
+                              letterSpacing: 1,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          widget.pageModel.description,
+                          style: TextStyle(
+                            fontSize: 18,
+                            color:
+                                widget.pageModel.accentColor.withOpacity(0.9),
+                          ),
                         ),
                       ),
                     ],
                   ),
-                )
+                ),
               ),
-            ]
+            ],
           ),
         ),
-        new Align(
+        Align(
           alignment: Alignment.centerRight,
           child: AnimatedBuilder(
             animation: borderAnimation,
-            child: new Align(
-                  alignment: Alignment.bottomCenter,
-                  child: new Padding(
-                    padding: const EdgeInsets.only(bottom: 24.0),
-                    child: new IconButton(
-                      icon: new Icon(
-                        Icons.arrow_back,
-                        color: widget.pageModel.primeColor,
-                        size: 32,
-                      ),
-                      onPressed: _nextButtonPressed(),
-                    ),
-                    ),
-                ),
-            builder: (BuildContext context, child){
-              return new CustomPaint(
-                painter: new DrawerPaint(
+            builder: (context, child) {
+              return CustomPaint(
+                painter: DrawerPaint(
                   curveColor: widget.pageModel.accentColor,
                 ),
-                child: child,
+                child: Container(
+                  width: borderAnimation.value,
+                  height: double.infinity,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 24.0),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: widget.pageModel.primeColor,
+                          size: 32,
+                        ),
+                        onPressed: _nextButtonPressed,
+                      ),
+                    ),
+                  ),
+                ),
               );
             },
           ),
