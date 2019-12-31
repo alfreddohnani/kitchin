41605ca5da1acdadc7f22e0ec91331d56031b6b2
mesteralfreddohnani <alfred.doh-nani@meltwater.org>
2019-12-31 13:43:02 +0000

    Remove subhead and description

--- lib/screens/onboarding/models/OnboardPageModel.dart
+++ lib/screens/onboarding/models/OnboardPageModel.dart
@@ -7,9 +7,7 @@ class OnboardPageModel {
   final int    pageNumber;
   final String imagePath;
   final String caption;
-  final String subhead;
-  final String description;
 
   OnboardPageModel(this.primeColor, this.accentColor, this.nextAccentColor, this.pageNumber, this.imagePath,
-      this.caption, this.subhead, this.description);
+      this.caption);
 }
